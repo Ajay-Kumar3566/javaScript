f0695359cbcd1df740da1e70cd15667328bd66a8
Ajay Kumar <av0270084@gmail.com>
2025-12-09 23:24:12 +0530

    Add date and time handling examples in JavaScript

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,27 @@
+// chaliye dekhte hai time kaisa chal rha haii...😂😂
+
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log( typeof myDate);//object hi hai
+
+
+let myCreateDate = new Date(2023, 0, 23)//in js month start with 0; 0->january...
+// console.log(myCreateDate.toDateString());
+// console.log();
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreateDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+
+
+
+
+
+
+
