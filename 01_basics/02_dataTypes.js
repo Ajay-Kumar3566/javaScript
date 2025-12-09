25a4376e0e3238874c1ca13e4ef880b9f82c49be
Ajay Kumar <av0270084@gmail.com>
2025-12-09 09:57:08 +0530

    Add data types discussion

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict";//treat this code as newer version ...
+
+// alert("hello")//we ae using node js not the browser here...
+
+
+//chaliye discuss kr lete hai dataTypes ke bare me ...
+
+
+/*
+number =>2 to power 53
+bigInt
+string =>""
+boolean=> true/false
+null => standAlone value(it is an object)
+undefined => not started yet..
+
+symbol=> unique
+object
+*/
+
+
+// ⭐⭐ pdhne wali chiz yhi hai aur sb to normal chiz hai...
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+console.log(null || undefined) // undefined
+console.log(null  && undefined); // null
+
+
