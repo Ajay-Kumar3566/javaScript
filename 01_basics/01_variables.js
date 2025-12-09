f74451855cef2ea928fd921e661c31adeac0bb15
Ajay Kumar <av0270084@gmail.com>
2025-12-09 08:13:11 +0530

    Add variable exploration script with console logging

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+// lets investigate about the variables
+
+// const , let , var
+const schema = 38992// can not be changed
+let accountId = "ajay@gmail.com"  // it is prefer to be used.
+
+
+var accountPass = "1234"// not used cause of scoping and functionality😒😒
+city = "Jaunpur"// kya hi gunda bnega re tu
+
+console.log(schema);
+
+// sbko ek sath print kra lete hai.. kya kehto ho😂
+// we use "console.table" to print all the values at a time .. it uses key value (indexing method)
+console.table([schema,accountId,accountPass,city])
