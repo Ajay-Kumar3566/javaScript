075c72bb51ccbf5a307abfb1bb23a82b9ec4dc2a
Ajay Kumar <av0270084@gmail.com>
2025-12-11 06:52:15 +0530

    Add examples of truthy and falsy values, nullish coalescing, and ternary operator in JavaScript

---
+++ 04_control_flow/03_truthy.js
@@ -0,0 +1,39 @@
+// const userEmail = "a@ajay.ai"
+
+// if (userEmail) {
+//     console.log("GOt user email");
+    
+// }else{
+//     console.log("Don't have user email");
+    
+// }
+
+
+// falsy values
+
+// false , 0, -0, bigInt 0n, "", null, undefined, NaN
+
+
+// truthy value..
+
+// "0",'false'," ",[],{},function(){}
+
+
+// Nullish Coalescing Operator (??) : null undefined
+
+let val1;
+// val1 = 5??10//5
+// val1 = null ?? 10//10
+// val1 = undefined ?? 15//15
+val1 = null ?? 10 ?? 20//10
+// console.log(val1);
+
+
+
+
+
+// Ternary Operator
+
+const price = 100
+price <= 80 ? console.log("it is good") : console.log("it is also good");
+
