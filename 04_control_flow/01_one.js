ac513c646ef3fb0d853e16a4f57303f74f896925
Ajay Kumar <av0270084@gmail.com>
2025-12-11 06:22:44 +0530

    Add examples of if statements and shorthand notation in JavaScript

---
+++ 04_control_flow/01_one.js
@@ -0,0 +1,32 @@
+//if statement....
+
+// if (condition) {
+//     // true then execute this block....
+// }
+
+if (2=="2") {
+    console.log("executed");
+    
+}
+if (2==="2") {
+    console.log("executed");
+    
+}
+
+
+// if else Statement...
+
+const score = 200
+if (score) {
+    let power ="fly"
+    console.log(`User power : ${power}`);
+    
+} 
+
+
+//shorthand notation....
+const balance = 1000
+if(balance > 500) console.log("test"),console.log("test2");
+
+
+// <,>,<=,>=,==,===,!= ,!==
