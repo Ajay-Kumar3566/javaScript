e0c4238c1b1e223efc0113424f38c3f5b46552a5
Ajay Kumar <av0270084@gmail.com>
2025-12-14 07:23:21 +0530

    Refactor User class to implement function-based and object-based getters and setters for email and password

--- 10_oops/10_getter_and_setter.js
+++ 10_oops/10_getter_and_setter.js
@@ -1,22 +1,75 @@
-class User {
-    constructor(email , password){
-        this.email = email
-        this.password = password
-    }
+// ✅✅getter and setter by using class..
+
+
+// class User {
+//     constructor(email , password){
+//         this.email = email
+//         this.password = password
+//     }
+//     get email(){
+//         return this._email.toUpperCase()
+//     }
+//     set email(value){
+//         this._email = value
+//     }
+//     get password(){
+//         return `${this._password}hitesh`
+//     }
+//     set password(value){
+//         this._password = value
+//     }
+// }
+// const hitesh = new User("ajidi@gmail.com", 898932)
+// console.log(hitesh.password);
+// console.log(hitesh.email);
+
+
+
+// ✅✅✅ function  based getter and setter 
+
+function User(email, password){
+    this._email = email;
+    this._password = password
+
+    Object.defineProperty(this, 'email', {
+        get: function(){
+            return this._email.toUpperCase()
+        },
+        set: function(value){
+            this._email = value
+        }
+    })
+    Object.defineProperty(this, 'password', {
+        get: function(){
+            return this._password.toUpperCase()
+        },
+        set: function(value){
+            this._password = value
+        }
+    })
+
+}
+
+const chai = new User("chai@chai.com", "chai")
+
+console.log(chai.email);
+
+
+// ✅✅ Object based getter and setter 
+
+const User = {
+    _email: 'h@hc.com',
+    _password: "abc",
+
+
     get email(){
         return this._email.toUpperCase()
-    }
+    },
+
     set email(value){
         this._email = value
     }
-    get password(){
-        return `${this._password}hitesh`
-    }
-    set password(value){
-        this._password = value
-    }
 }
-const hitesh = new User("ajidi@gmail.com", 898932)
-console.log(hitesh.password);
-console.log(hitesh.email);
 
+const tea = Object.create(User)
+console.log(tea.email);
